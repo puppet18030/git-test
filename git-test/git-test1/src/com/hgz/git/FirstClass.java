385bf2bad7f38cabbb2620ea92b520c904d9cf0b
LaoHu <LaoHu@163.com>
2021-08-10 22:03:49 +0800

    练习idea提交项目

---
+++ git-test/git-test1/src/com/hgz/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.hgz.git;
+
+/**
+ * @Classname FirstClass
+ * @Description TODO
+ * @Date 2021/8/10 21:50
+ * @Created by 小布
+ */
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
